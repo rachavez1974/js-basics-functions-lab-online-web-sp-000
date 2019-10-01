cbd0e3ef6ea277059d74693a26656081dd71a774
Ramon Chavez <rachavez1974@gmail.com>
2019-09-30 21:48:45 -0400

    Finish js-basics-functions-lab-online-web-sp-000

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blockNumber) {
+  return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  
+  if (distance < 400){
+    return 0;
+  }
+  else if (distance > 400 && distance < 2000){
+    return (distance - 400) * .02
+  }else if (distance > 2000 && distance < 2500){
+    return 25;
+  }else if (distance > 2500) {
+    return 'cannot travel that far';
+  }
+}
